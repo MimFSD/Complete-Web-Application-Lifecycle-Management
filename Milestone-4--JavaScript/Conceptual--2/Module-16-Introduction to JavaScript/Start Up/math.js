7d8bea060818652bce5669ba11960d0e52d3281e
Mim Islam <155841383+MimFSD@users.noreply.github.com>
2024-08-30 00:27:18 +0600

    Add files via upload

---
+++ Milestone-4--JavaScript/Conceptual--2/Module-16-Introduction to JavaScript/Start Up/math.js
@@ -0,0 +1,8 @@
+var height =38;
+console.log(height);
+var price=22;
+var year=1999;
+var book=73;
+var age; //undefined
+console.log(price, year,book,age);
+
---
+++ Milestone-4--JavaScript/Conceptual--2/Module-16-Introduction to JavaScript/Start Up/number.js
@@ -0,0 +1,39 @@
+//sum
+var mango_Price= 160;
+var orange_Price=320;
+total= mango_Price + orange_Price;
+console.log(total);
+//another method
+console.log(mango_Price+orange_Price);
+var door=17;
+var window="15"; // this coutation define as a string 
+console.log(door+window); //output : 1715
+console.log(typeof door, typeof window);
+
+var design = 19;
+var analysis= parseInt("22")
+console.log(typeof design, typeof analysis);
+
+var designR = 19.7654;
+var analysisR= parseFloat("67.4779") 
+console.log(typeof designR, typeof analysisR);
+console.log(designR+analysisR); // correct sum
+
+var first=0.1;
+var second=0.2;
+var total=first+second;
+console.log(total.toFixed(7));
+
+const num = 50/0; 
+const num2 = -50/0; 
+console.log(num,num2);
+
+console.log(5-'hello');  //Not a Number
+
+var s=7;
+var po=Number(s);
+console.log(po);
+
+var h=isNaN(34);
+console.log(h);
+
