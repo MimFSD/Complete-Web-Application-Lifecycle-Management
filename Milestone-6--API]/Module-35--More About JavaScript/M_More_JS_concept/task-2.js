7f2c951f970b91df4cdf3917f1c203042d531f45
Mim Islam <155841383+MimFSD@users.noreply.github.com>
2024-09-01 00:07:59 +0600

    More About JavaScript

---
+++ Milestone-6--API]/Module-35--More About JavaScript/M_More_JS_concept/task-2.js
@@ -0,0 +1,18 @@
+// Task - 2
+// The function tellJoke() below logs a funny message every 2 seconds and after 10 seconds, it stops. Complete the code below and see the output.
+
+//     function tellJoke() {
+//      console.log("Why don't scientists trust atoms? Because they make up
+//      everything!");
+//     }
+
+//     const jokeInterval = setInterval(?, ?);
+
+//     // After 10 seconds, stop telling jokes
+
+//     setTimeout(() => {
+
+//      //write code
+
+//     }, ?);
+// // 
