a3fcde97ff0c00f551c431c449e67a1d2c167488
Mim Islam <155841383+MimFSD@users.noreply.github.com>
2024-09-02 00:54:07 +0600

    Tailwind CSS, Axios, Rechart, Awesome Components

---
+++ Milestone-8--React-Rounter/Module 44 Tailwind CSS, Axios, Rechart, Awesome Components/my-price-option/src/components/daisyNav.jsx
@@ -0,0 +1,14 @@
+// React Extension Pack
+// Reactjs code snippets
+// ES7+ React/Redux/React-Native snippets
+import React, { Component } from 'react'
+
+export class daisyNav extends Component {
+  render() {
+    return (
+      <div>daisyNav</div>
+    )
+  }
+}
+
+export default daisyNav
