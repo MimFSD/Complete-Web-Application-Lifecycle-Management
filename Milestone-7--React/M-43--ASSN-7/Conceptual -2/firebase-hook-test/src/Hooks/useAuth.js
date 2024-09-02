644e3b8d053330bc6fc5cc577c73c8479ce035f7
Mim Islam <155841383+MimFSD@users.noreply.github.com>
2024-09-03 00:04:18 +0600

    remove firebase-hook-test directory

--- Milestone-7--React/M-43--ASSN-7/Conceptual -2/firebase-hook-test/src/Hooks/useAuth.js
+++
@@ -1,7 +0,0 @@
-import { AuthContext } from "../components/Context/AuthProvider";
-import { useContext } from "react";
-const useAuth = () => {
-  return useContext(AuthContext);
-};
-
-export default useAuth;
