34556f70af97d84579164f78487423db44ff073c
Mim Islam <155841383+MimFSD@users.noreply.github.com>
2024-09-03 00:05:30 +0600

    Auth Integration And Private Route/

---
+++ Milestone-9--React Authentication/Module 51--React Auth Integration And Private Route/my-auth-project-moha/src/firebase/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAs33R8DS-G_XOVRhqPSkiSjO8euZc9lWg",
+  authDomain: "my-auth-project-moha.firebaseapp.com",
+  projectId: "my-auth-project-moha",
+  storageBucket: "my-auth-project-moha.appspot.com",
+  messagingSenderId: "181322965440",
+  appId: "1:181322965440:web:877311a40710229e0f6825"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
